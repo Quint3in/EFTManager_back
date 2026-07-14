373941a707b254c896e993c66d13ea1145970e55
Joel Quintana <joelqn95@gmail.com>
2026-07-14 12:37:28 +0200

    Init commit

---
+++ src/main/java/cat/itacademy/s05/t02/eftmanager/EftmanagerApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t02.eftmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EftmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EftmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/s05/t02/eftmanager/EftmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t02.eftmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EftmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
